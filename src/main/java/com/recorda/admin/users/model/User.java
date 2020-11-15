ffd7f875eed4aab621681c148293179284ce01d0
SylvainRichet <richet.sylvain@gmail.com>
2020-11-15 13:27:35 +0100

    Cleanin mongodb collection prior to run is useless #2

--- src/main/java/com/recorda/admin/users/model/User.java
+++ src/main/java/com/recorda/admin/users/model/User.java
@@ -1,6 +1,7 @@
 package com.recorda.admin.users.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -30,7 +31,8 @@ public class User implements Serializable {
 
     private String address;
 
-    @Transient /* do not persist IP field */
+    @Transient  /* do not persist IP field */
+    @JsonIgnore /* do not display field */
     private String ip;
 
     @Override
